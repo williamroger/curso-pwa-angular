fc447271e7c69ec6ef3f7c00310ada7c9ae21844
William Roger <williamroger.frontend@gmail.com>
2020-12-30 11:22:40 -0300

    Adicionados Models MarcaCarro e Seguro e finalizado serviço MarcaCarro

---
+++ src/app/models/Seguro.ts
@@ -0,0 +1,10 @@
+import { MarcaCarro } from './MarcaCarro';
+
+export class Seguro {
+  marcaCarro: MarcaCarro;
+  modeloCarro: string;
+  placaCarro: string;
+  nomeProprietario: string;
+  sobrenomeProprietario: string;
+  dataNascimentoProprietario: string;
+}
---
+++ src/app/services/marca-carro.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { MarcaCarro } from './../models/MarcaCarro';
+
+interface CarResponse {
+  Makes: Array<any>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarcaCarroService {
+  private API_CARROS = 'http://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes';
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  private mapMarcas(marcas ): MarcaCarro[] {
+    return marcas.map(marca => ({
+      codigo: marca.make_id,
+      nome: marca.make_display,
+    }));
+  }
+
+  public getMarcas(): Observable<MarcaCarro[]> {
+    return this.http.jsonp(this.API_CARROS, 'callback')
+      .pipe(
+        map((res: CarResponse) => this.mapMarcas(res.Makes))
+      );
+  }
+}
